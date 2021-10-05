7e350d7fdbdd4376e6d19a72c9b5e646e2a203e5
Simon Pettersson <simon.v.pettersson@gmail.com>
2021-10-05 19:13:07 +0200

    streamline exa

--- users/profiles/exa/default.nix
+++ users/profiles/exa/default.nix
@@ -1,17 +1,21 @@
 { pkgs
-, lib
 , ...
 }:
 let
+  common = "--header --git --icons";
   aliases = {
-    ll = lib.mkForce "${pkgs.exa}/bin/exa --long --header --git";
-    lt = lib.mkForce "${pkgs.exa}/bin/exa --long --header --git --tree";
+    ls = "${pkgs.exa}/bin/exa ${common} --git-ignore";
+    la = "${pkgs.exa}/bin/exa ${common} --all";
+    ll = "${pkgs.exa}/bin/exa ${common} --long --git-ignore";
+    lla = "${pkgs.exa}/bin/exa ${common} --long --all";
+    lt = "${pkgs.exa}/bin/exa ${common} --tree --git-ignore";
+    lta = "${pkgs.exa}/bin/exa ${common} --tree --all";
   };
 in
 {
   config.programs.exa = {
     enable = true;
-    enableAliases = true;
+    enableAliases = false;
   };
   config.programs.bash.shellAliases = aliases;
   config.programs.zsh.shellAliases = aliases;
