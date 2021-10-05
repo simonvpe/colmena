20b61f1a5414e140b7319249737246b247816102
Simon Pettersson <simon.v.pettersson@gmail.com>
2021-10-05 18:51:26 +0200

    nicer ll alias

--- users/profiles/exa/default.nix
+++ users/profiles/exa/default.nix
@@ -1,6 +1,18 @@
+{ pkgs
+, lib
+, ...
+}:
+let
+  aliases = {
+    ll = lib.mkForce "${pkgs.exa}/bin/exa --long --header --git";
+  };
+in
 {
   config.programs.exa = {
     enable = true;
     enableAliases = true;
   };
+  config.programs.bash.shellAliases = aliases;
+  config.programs.zsh.shellAliases = aliases;
+  config.programs.fish.shellAliases = aliases;
 }
