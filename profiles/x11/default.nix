ad2f3fcc3f6aa30d36417c6ccee44ec56b19a242
Simon Pettersson <simon.pettersson@rco.se>
2021-10-28 23:25:51 +0200

    fixes telephone symbol for good

--- profiles/x11/default.nix
+++ profiles/x11/default.nix
@@ -16,6 +16,8 @@
     fontDir.enable = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
+      # Beautiful and free fonts for many languages
+      noto-fonts
       # Microsoft's TrueType core fonts for the Web
       corefonts
       # Iconic font aggregator, collection, & patcher. 3,600+ icons, 50+ patched fonts
@@ -24,8 +26,8 @@
       meslo-lgs-nf
     ];
     fontconfig.defaultFonts = {
-      serif = [ "NotoSerif Nerd Font" ];
-      sansSerif = [ "NotoSerif Nerd Font" ];
+      serif = [ "Noto Serif" "Noto Serif Nerd Font" ];
+      sansSerif = [ "Noto Sans" "Noto Sans Nerd Font" ];
       monospace = [ "MesloLGS Nerd Font Mono" ];
     };
   };
