f2b5982067ceda8a03839039baae0d53e54fb37e
Simon Pettersson <simon.pettersson@rco.se>
2021-11-01 00:07:12 +0100

    adds hop

--- users/profiles/vim/navigation.nix
+++ users/profiles/vim/navigation.nix
@@ -1,8 +1,13 @@
 { config
+, pkgs
 , ...
 }:
 
 {
+  config.programs.neovim.plugins = with pkgs.vimPlugins; [
+    hop-nvim
+  ];
+
   config.programs.neovim.extraConfig = ''
     " Automatically set the current working directory to the file
     " you're on
@@ -13,5 +18,14 @@
     :nnoremap <silent> tc :tabclose<CR>
     :nnoremap <silent> th :tabprevious<CR>
     :nnoremap <silent> tl :tabnext<CR>
+
+    " Hop (normal)
+    :nnoremap <silent> hl :HopLine<CR>
+    :nnoremap <silent> hw :HopWord<CR>
+
+    " Hop (visual) !NOT WORKING
+    " :xnoremap <silent> <C-h>l :<C-U>HopLine<CR>
+    " :xnoremap <silent> <C-h>w :<C-U>HopWord<CR>
+
   '';
 }
