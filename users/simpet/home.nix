4de1693378ec6c69051c57466c3ece8d330c4ef6
Simon Pettersson <simpet@netinsight.net>
2021-11-16 12:58:57 +0100

    add ssh config and .bash_profile to user simpet

--- users/simpet/home.nix
+++ users/simpet/home.nix
@@ -3,7 +3,50 @@
   config = {
     programs.git.userName = "Simon Pettersson";
     programs.git.userEmail = "simpet@netinsight.net";
-    programs.ssh.extraConfig = "Include ~/.ssh/config.neti";
+    programs.ssh.enable = true;
+    programs.ssh.extraConfig = ''
+      Host git
+        IdentityFile ~/.ssh/id_ed25519
+        User gitosis
+        ForwardX11 no
+
+      Host gaston0
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston0.lab.netinsight.se
+          ForwardX11 no
+
+      Host gaston1
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston1.lab.netinsight.se
+          ForwardX11 no
+
+      Host gaston2
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston2.lab.netinsight.se
+          ForwardX11 no
+
+      Host gaston3
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston3.lab.netinsight.se
+          ForwardX11 no
+
+      Host gaston4
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston4.lab.netinsight.se
+          ForwardX11 no
+
+      Host gaston5
+          IdentityFile ~/.ssh/id_ed25519
+          User root
+          HostName gaston5.lab.netinsight.se
+          ForwardX11 no
+    '';
+    # programs.ssh.extraConfig = "Include ~/.ssh/config.neti";
     # age = {
     #   enable = true;
     #   uid = 1002;
@@ -30,5 +73,12 @@
     #   ];
     # };
     # systemd.user.startServices = true;
+
+    home.file.".bash_profile".text = ''
+      if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+      fi
+    '';
   };
+
 }
