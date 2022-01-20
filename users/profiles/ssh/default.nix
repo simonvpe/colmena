ffe9a66ca17f93e986c05015663426e1d02e0d8a
Simon Pettersson <simon.v.pettersson@gmail.com>
2022-01-20 12:18:43 +0100

    use ssh control paths

--- users/profiles/ssh/default.nix
+++ users/profiles/ssh/default.nix
@@ -3,4 +3,9 @@
   programs.ssh.forwardAgent = true;
   programs.ssh.serverAliveCountMax = 10;
   programs.ssh.serverAliveInterval = 60;
+  programs.ssh.extraConfig = ''
+    # re-use ssh connections
+    Host *
+      ControlPath ~/.ssh/control-%h-%p-%r
+  '';
 }
