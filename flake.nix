787af8329adf3922dec15855a38e9e1e4a4b85e3
Simon Pettersson <simon.v.pettersson@gmail.com>
2022-03-05 09:50:20 +0100

    change to stable nixos

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   inputs =
     {
-      nixos.url = "github:nixos/nixpkgs/nixos-unstable";
+      nixos.url = "github:nixos/nixpkgs/21.11";
       latest.url = "github:nixos/nixpkgs/nixos-unstable";
 
       digga.url = "github:divnix/digga";
