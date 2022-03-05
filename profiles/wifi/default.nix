80f67e3ae4cfb797b3084781a6d1666c2097bcdd
Simon Pettersson <simon.v.pettersson@gmail.com>
2022-03-05 09:30:30 +0100

    configure wifi access points

--- profiles/wifi/default.nix
+++ profiles/wifi/default.nix
@@ -8,10 +8,14 @@
     Finkontoret.psk = "strosaitrosa";
     OWNIT-5GHz_BE20.psk = "Q72CRXMKP7BXYL"; # Elins
     SW.psk = "#saltyaf";
-    OnePlus3.psk = "pappapappa";
+    OnePlus3 = {
+      psk = "pappapappa";
+      priority = 100;
+    };
     #"513F8F".psk = "c9mior2jcy";
     Bolema.psk = "Split053";
     Eckero_public = { };
     Inpro.psk = "Brunnsvatten";
+    Viking_Public = { };
   };
 }
